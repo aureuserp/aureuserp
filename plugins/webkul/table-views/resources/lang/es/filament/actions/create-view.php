0a29c629321b7a3849130a7f5aba1e8d8d073d10
Ramiro Brescia <ramirobrescia@gmail.com>
2025-06-21 08:40:05 -0300

    Table Views module localization in Spanish

---
+++ plugins/webkul/table-views/resources/lang/es/filament/actions/create-view.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'label' => 'Añadir Nuevo',
+
+    'form' => [
+        'name'                  => 'Nombre',
+        'color'                 => 'Color',
+        'icon'                  => 'Icono',
+        'add-to-favorites'      => 'Añadir a Favoritos',
+        'add-to-favorites-help' => 'Añadir este filtro a tus favoritos',
+        'make-public'           => 'Hacer Público',
+        'make-public-help'      => 'Hacer este filtro disponible para todos los usuarios',
+        'options'               => [
+            'danger'  => 'Peligro',
+            'gray'    => 'Gris',
+            'info'    => 'Información',
+            'success' => 'Éxito',
+            'warning' => 'Advertencia',
+        ],
+
+        'notification' => [
+            'created' => 'Vista creada exitosamente',
+        ],
+
+        'modal' => [
+            'title' => 'Guardar Vista',
+        ],
+    ],
+];
---
+++ plugins/webkul/table-views/resources/lang/es/filament/actions/edit-view.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'form' => [
+        'name'                  => 'Nombre',
+        'color'                 => 'Color',
+        'icon'                  => 'Icono',
+        'add-to-favorites'      => 'Añadir a Favoritos',
+        'add-to-favorites-help' => 'Añadir este filtro a tus favoritos',
+        'make-public'           => 'Hacer Público',
+        'make-public-help'      => 'Hacer este filtro disponible para todos los usuarios',
+        'options'               => [
+            'danger'  => 'Peligro',
+            'gray'    => 'Gris',
+            'info'    => 'Información',
+            'success' => 'Éxito',
+            'warning' => 'Advertencia',
+        ],
+
+        'notification' => [
+            'created' => 'Vista creada exitosamente', // Nota: Aunque es "Editar Vista", la notificación dice "creada". Es probable un error en el original o se reutiliza.
+        ],
+
+        'modal' => [
+            'title' => 'Editar Vista',
+        ],
+    ],
+];
