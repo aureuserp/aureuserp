8e1305c4b392c9b1861282541a832fda55f2655b
Jitendra <jitendra@webkul.in>
2025-10-10 14:15:16 +0530

    Extended currency resource in accounting plugin

---
+++ plugins/webkul/accounting/src/Filament/Clusters/Configuration/Resources/CurrencyResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Webkul\Accounting\Filament\Clusters\Configuration\Resources;
+
+use Webkul\Accounting\Filament\Clusters\Configuration;
+use Webkul\Accounting\Filament\Clusters\Configuration\Resources\CurrencyResource\Pages\ListCurrencies;
+use Webkul\Accounting\Filament\Clusters\Configuration\Resources\CurrencyResource\Pages\CreateCurrency;
+use Webkul\Accounting\Filament\Clusters\Configuration\Resources\CurrencyResource\Pages\EditCurrency;
+use Webkul\Accounting\Filament\Clusters\Configuration\Resources\CurrencyResource\Pages\ViewCurrency;
+use Webkul\Support\Filament\Resources\CurrencyResource as BaseCurrencyResource;
+
+class CurrencyResource extends BaseCurrencyResource
+{
+    protected static bool $shouldRegisterNavigation = true;
+
+    protected static ?string $cluster = Configuration::class;
+
+    public static function getModelLabel(): string
+    {
+        return __('accounting::filament/clusters/configurations/resources/currency.model-label');
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return __('accounting::filament/clusters/configurations/resources/currency.navigation.title');
+    }
+
+    public static function getNavigationGroup(): string
+    {
+        return __('accounting::filament/clusters/configurations/resources/currency.navigation.group');
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListCurrencies::route('/'),
+            'create' => CreateCurrency::route('/create'),
+            'edit' => EditCurrency::route('/{record}/edit'),
+            'view' => ViewCurrency::route('/{record}'),
+        ];
+    }
+}
