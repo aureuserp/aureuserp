c67bb2a6f6890542f472efbae9d16b1dbcadc974
Jitendra <jitendra@webkul.in>
2025-12-30 19:55:18 +0530

    Fixed sales settings issues

--- plugins/webkul/sales/src/Filament/Clusters/Settings/Pages/ManageProducts.php
+++ plugins/webkul/sales/src/Filament/Clusters/Settings/Pages/ManageProducts.php
@@ -26,6 +26,11 @@ class ManageProducts extends SettingsPage
 
     protected static ?string $cluster = Settings::class;
 
+    protected static function getPagePermission(): ?string
+    {
+        return 'page_sale_manage_products';
+    }
+
     public function getBreadcrumbs(): array
     {
         return [
