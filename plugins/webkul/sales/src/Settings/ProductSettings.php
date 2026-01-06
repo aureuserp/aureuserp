59f3e5ef907ca0dc653770c608328c28e71a92b7
Jitendra <jitendra@webkul.in>
2026-01-06 18:39:12 +0530

    Fixed migration issues

--- plugins/webkul/sales/src/Settings/ProductSettings.php
+++ plugins/webkul/sales/src/Settings/ProductSettings.php
@@ -7,4 +7,9 @@ use Webkul\Product\Settings\ProductSettings as BaseProductSettings;
 class ProductSettings extends BaseProductSettings
 {
     public bool $enable_deliver_content_by_email;
+
+    public static function group(): string
+    {
+        return 'sales_product';
+    }
 }
