d4ffe5a086c951a74287bbe35c4013af65b3624f
harshqa-webkul <harshgupta.qa322@webkul.in>
2026-01-29 19:47:13 +0530

    plugin install/uninstall using POM

--- tests/e2e-pw/tests/pluginSetup.spec.ts
+++ tests/e2e-pw/tests/pluginSetup.spec.ts
@@ -1,79 +1,27 @@
 import { test, expect } from '../setup';
+import { PluginManagementPage } from '../pages/pluginmanagement';
 
 test.describe('Plugin Installation', () => {
-    let j = 0;
 
     test.beforeEach(async ({ adminPage }) => {
-
-        await adminPage.goto('/admin/plugins');
-        await expect(adminPage).toHaveURL(/.*admin/);
-        await expect(adminPage.locator('text=Sync Available Plugins')).toBeVisible();
+        const pluginManagementPage = new PluginManagementPage(adminPage);
+        await pluginManagementPage.gotoPluginManagementPage();
     });
 
-    /** 
+    /**
      * All plugins installation test
      */
     test('All Plugins Installation Test', async ({ adminPage }) => {
         test.setTimeout(400000);
-        for (let i = 0; i < 13; i++) {
-
-            const title = adminPage.locator('.fi-size-lg.fi-font-semibold.fi-ta-text-item.fi-ta-text.fi-inline');
-            await adminPage.locator('button[title="Actions"]').nth(i).click();
-            const checkInstalled = await adminPage.locator('button.fi-color.fi-color-danger.fi-dropdown-list-item').nth(i).isVisible();
-
-            if (!checkInstalled) {
-                await adminPage.waitForLoadState('networkidle');
-                await adminPage.locator('button.fi-color.fi-color-success.fi-text-color-700').nth(j).click();
-                await adminPage.waitForTimeout(3000); // Wait for 3 seconds to allow installation to complete
-                await adminPage.locator('span[x-show="! isProcessing"]').click();
-                const pluginTitle = await title.nth(i).innerText();
-                console.log(`Installing Plugin: ${pluginTitle}`);
-                await expect(adminPage.getByRole('heading', { name: 'Plugin Installed Successfully' })).toBeVisible();
-            }
-        }
+        const pluginManagementPage = new PluginManagementPage(adminPage);
+        await pluginManagementPage.installPlugin();
     });
 
-    /** 
-     * All plugins installation test
+    /**
+     * All plugins uninstallation test
      */
-    test('All Plugins Uninstallation Test', async ({ adminPage }) => {
-        for (let i = 0; i < 13; i++) {
-
-            const title = adminPage.locator('.fi-size-lg.fi-font-semibold.fi-ta-text-item.fi-ta-text.fi-inline');
-            await adminPage.locator('button[title="Actions"]').nth(i).click();
-            const checkInstalled = await adminPage.locator('button.fi-color.fi-color-danger.fi-dropdown-list-item').nth(j).isVisible();
-
-            if (checkInstalled) {
-                await adminPage.waitForLoadState('networkidle');
-                await adminPage.waitForTimeout(2000);
-                await adminPage.locator('button.fi-color.fi-color-danger.fi-dropdown-list-item').nth(j).click();
-                await adminPage.waitForTimeout(5000);
-                await adminPage.locator('span[x-show="! isProcessing"]').click();
-                // await expect(adminPage.getByText('Plugin Uninstalled Successfully')).toBeVisible();
-                const pluginTitle = await title.nth(i).innerText();
-                console.log(`Uninstalling Plugin: ${pluginTitle}`);
-                await expect(adminPage.getByRole('heading', { name: 'Plugin Uninstalled Successfully' })).toBeVisible();
-            }
-        }
+    test('All Plugins Uninstallation Test', async ({  adminPage }) => {
+        const pluginManagementPage = new PluginManagementPage(adminPage);
+        await pluginManagementPage.uninstallPlugin();
     });
-
-    /** 
-     * Plugins installation for testing purposes
-     */
-    // test('Plugins Installation for testing purposes', async ({ adminPage }) => {
-    //     for (let i = 0; i < 13; i++) {
-
-    //         await adminPage.locator('button[title="Actions"]').nth(i).click();
-    //         const checkInstalled = await adminPage.locator('button.fi-color.fi-color-danger.fi-dropdown-list-item').nth(i).isVisible();
-
-    //         if (!checkInstalled) {
-    //             await adminPage.waitForLoadState('networkidle');
-    //             await adminPage.locator('button.fi-color.fi-color-success.fi-text-color-700').nth(j).click();
-    //             await adminPage.waitForTimeout(5000); // Wait for 5 seconds to allow installation to complete
-    //             await adminPage.locator('span[x-show="! isProcessing"]').click();
-    //             // await expect(adminPage.getByText('Plugin Installed Successfully')).toBeVisible();
-    //             await expect(adminPage.getByRole('heading', { name: 'Plugin Installed Successfully' })).toBeVisible();
-    //         }
-    //     }
-    // });
 });
