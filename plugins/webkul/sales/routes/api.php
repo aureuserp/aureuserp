ebf63231dc12d717e264b5258ea43f48f07b7963
Jitendra <jitendra@webkul.in>
2026-02-04 16:43:00 +0530

    Added packging api

--- plugins/webkul/sales/routes/api.php
+++ plugins/webkul/sales/routes/api.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Webkul\Sale\Http\Controllers\API\V1\AttributeController;
 use Webkul\Sale\Http\Controllers\API\V1\AttributeOptionController;
-use Webkul\Sale\Http\Controllers\API\V1\CategoryController;
+use Webkul\Sale\Http\Controllers\API\V1\OrderController;
+use Webkul\Sale\Http\Controllers\API\V1\OrderLineController;
+use Webkul\Sale\Http\Controllers\API\V1\PackagingController;
 use Webkul\Sale\Http\Controllers\API\V1\ProductAttributeController;
 use Webkul\Sale\Http\Controllers\API\V1\ProductController;
 use Webkul\Sale\Http\Controllers\API\V1\ProductVariantController;
-use Webkul\Sale\Http\Controllers\API\V1\OrderController;
-use Webkul\Sale\Http\Controllers\API\V1\OrderLineController;
 
 Route::name('admin.api.v1.sales.')->prefix('admin/api/v1/sales')->middleware(['auth:sanctum'])->group(function () {
     Route::softDeletableApiResource('attributes', AttributeController::class);
@@ -20,6 +20,8 @@ Route::name('admin.api.v1.sales.')->prefix('admin/api/v1/sales')->middleware(['a
     Route::apiResource('products.attributes', ProductAttributeController::class);
 
     Route::softDeletableApiResource('products.variants', ProductVariantController::class);
+
+    Route::softDeletableApiResource('packagings', PackagingController::class);
 
     Route::apiResource('orders', OrderController::class);
 
