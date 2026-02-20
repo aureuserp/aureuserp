c93215253405cbb546f24938c69cc5879efec9e5
Suraj Kashyap <suraj.kashyap370@webkul.in>
2026-02-20 13:03:24 +0530

    Issue #1055 has been fixed.

--- plugins/webkul/security/src/Filament/Resources/CompanyResource/RelationManagers/BranchesRelationManager.php
+++ plugins/webkul/security/src/Filament/Resources/CompanyResource/RelationManagers/BranchesRelationManager.php
@@ -151,17 +151,16 @@ class BranchesRelationManager extends RelationManager
                                     ->schema([
                                         Select::make('currency_id')
                                             ->relationship(
-                                                'currency',
-                                                'full_name',
-                                                modifyQueryUsing: fn (Builder $query) => $query->where('active', 1),
+                                                name: 'currency',
+                                                titleAttribute: 'full_name',
+                                                modifyQueryUsing: fn (Builder $query) => $query->active(),
                                             )
                                             ->label(__('security::filament/resources/company/relation-managers/manage-branch.form.tabs.address-information.sections.additional-information.fields.default-currency'))
-                                            ->relationship('currency', 'full_name')
                                             ->searchable()
                                             ->required()
                                             ->live()
                                             ->preload()
-                                            ->default(Currency::first()?->id)
+                                            ->default(Currency::active()->first()?->id)
                                             ->createOptionForm([
                                                 Section::make()
                                                     ->schema([
