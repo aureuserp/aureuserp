cc19bb3f675213b8ffde662e32d960fd6b79ba04
Ryadh <madjid.gacem@hotmail.com>
2026-03-07 05:23:34 +0100

    adding the French translation to the project plugin

---
+++ plugins/webkul/projects/resources/lang/fr/filament/pages/dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'navigation' => [
+        'title' => 'Projet',
+        'group' => 'Tableau de Bord',
+    ],
+
+    'filters-form' => [
+        'project'    => 'Projet',
+        'assignees'  => 'Assignés',
+        'tags'       => 'Étiquettes',
+        'customer'   => 'Client',
+        'start-date' => 'Date de Début',
+        'end-date'   => 'Date de Fin',
+    ],
+];
