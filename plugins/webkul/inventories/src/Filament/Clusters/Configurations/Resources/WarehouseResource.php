399f50bd9505542010ee58a53a8d7fc3b7c46a0f
Bheem Kumar Sharma <bheem.sharma322@webkul.in>
2026-03-31 11:08:25 +0530

    fix: issue #1154 has been fixed

--- plugins/webkul/inventories/src/Filament/Clusters/Configurations/Resources/WarehouseResource.php
+++ plugins/webkul/inventories/src/Filament/Clusters/Configurations/Resources/WarehouseResource.php
@@ -379,23 +379,6 @@ class WarehouseResource extends Resource
         return once(fn () => app(WarehouseSettings::class));
     }
 
-    public static function getSubNavigationPosition(): SubNavigationPosition
-    {
-        $route = request()->route()?->getName() ?? session('current_route');
-
-        if ($route && $route != 'livewire.update') {
-            session(['current_route' => $route]);
-        } else {
-            $route = session('current_route');
-        }
-
-        if ($route === self::getRouteBaseName().'.index') {
-            return SubNavigationPosition::Start;
-        }
-
-        return SubNavigationPosition::Top;
-    }
-
     public static function getRecordSubNavigation(Page $page): array
     {
         return $page->generateNavigationItems([
