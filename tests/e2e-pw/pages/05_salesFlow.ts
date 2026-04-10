2a3f5dcf8fb9263ccb9e4369109c01fa23a43d50
harshqa-webkul <harshgupta.qa322@webkul.in>
2026-04-10 15:12:16 +0530

    fix: fixed formatting

--- tests/e2e-pw/pages/05_salesFlow.ts
+++ tests/e2e-pw/pages/05_salesFlow.ts
@@ -219,22 +219,6 @@ export class SalesFlowPage {
         return quotationId;
     }
 
-    // async openDeliveriesForCurrentQuotation(): Promise<string> {
-    //     const url = this.page.url();
-    //     const match = url.match(/quotations\/(\d+)/);
-
-    //     if (!match) {
-    //         throw new Error(`Unable to determine quotation id from URL: ${url}`);
-    //     }
-
-    //     const quotationId = match[1];
-    //     await this.page.waitForLoadState("networkidle");
-    //     await this.page.goto(`/admin/sale/orders/quotations/${quotationId}/deliveries`, { waitUntil: "networkidle" });
-    //     await expect(this.page).toHaveURL(new RegExp(`/quotations/${quotationId}/deliveries`));
-
-    //     return quotationId;
-    // }
-
     async openDeliveriesForCurrentQuotation(): Promise<string> {
         const url = this.page.url();
         const match = url.match(/quotations\/(\d+)/);
